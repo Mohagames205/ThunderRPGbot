4a70b5aec865600ee437d90bd693156d4a1d1329
Mohagames205 <40402787+Mohagames205@users.noreply.github.com>
2018-06-20 19:55:49 +0200

    Updated bot.py
    Removed unnecessary variables and libraries from the bot code

--- bot.py
+++ bot.py
@@ -6,23 +6,14 @@ import random
 import json
 import os
 import datetime
-#tijd = int(time.strftime("%M"))
-#tijd1 = str(time.strftime("%c"))
-#tijd1 = datetime.datetime.now().strftime("%c")
+
 cp = '&'
 bot = commands.Bot(command_prefix=cp)
 
 print ('Bot is aan het laden...')
-print ('.....')
-print ('Het laden is bijna compleet!')
-print ('Bij errors exit invoeren in console!')
 print ('De command prefix die wordt gebruikt is ' + cp)
-print ('...')
-print ('...')
-print ('We zijn er bijna!')
-print ('Ter info! De bot is niet bedoeld als U-bot maar als Discord Bot')
-print ('Versie 1.3')
-print ('Nieuwe logo en naam')
+print ('Versie 2.2')
+
 @bot.event
 async def on_ready():
     await bot.edit_profile(username="GreyStripe")
