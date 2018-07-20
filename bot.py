ab6ffd5771d45c79d5e117d001653231cc38d04c
Mohagames205 <40402787+Mohagames205@users.noreply.github.com>
2018-07-21 00:40:33 +0200

    patch for the Hi command

--- bot.py
+++ bot.py
@@ -64,7 +64,7 @@ async def tutorial_uptime():
 async def on_message(message):
 	if message.content.upper().startswith('YEET'):
 		await bot.send_message(message.channel, "Dabs on you!")
-	if message.content.upper().startswith('hi'):
+	if message.content.upper().startswith('HI'):
 		await bot.say("Hi how are u doing?")
 	await bot.process_commands(message)
 	
