6682d5e99c9962dcf0457ced13999d09d86bcf16
Mohagames205 <40402787+Mohagames205@users.noreply.github.com>
2018-07-21 15:37:05 +0200

    t

--- bot.py
+++ bot.py
@@ -169,5 +169,34 @@ async def info(ctx):
 async def exc(ctx, *, c):
 	await eval(c)
 	
+@commands.command(pass_context=True, hidden=True)
+@checks.is_owner()
+async def debug(self, ctx, *, code : str):
+    """Evaluates code."""
+    code = code.strip('` ')
+    python = '```py\n{}\n```'
+    result = None
+
+    env = {
+        'bot': self.bot,
+        'ctx': ctx,
+        'message': ctx.message,
+        'server': ctx.message.server,
+        'channel': ctx.message.channel,
+        'author': ctx.message.author
+    }
+
+    env.update(globals())
+
+    try:
+        result = eval(code, env)
+        if inspect.isawaitable(result):
+            result = await result
+    except Exception as e:
+        await self.bot.say(python.format(type(e).__name__ + ': ' + str(e)))
+        return
+
+    await self.bot.say(python.format(result))
+	
 bot.loop.create_task(tutorial_uptime())	
 bot.run(os.getenv('TOKEN'))
